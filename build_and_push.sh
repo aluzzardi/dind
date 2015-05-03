f7bd86cecbaa6f135137d08ed52cfc4b97887da6
Andrea Luzzardi <aluzzardi@gmail.com>
2015-05-02 18:16:05 -0700

    display commands that are run
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- build_and_push.sh
+++ build_and_push.sh
@@ -10,22 +10,27 @@ DOCKER_IMAGE=${DOCKER_IMAGE:-dockerswarm/dind}
 CURRENT_BRANCH=$(git symbolic-ref HEAD | sed s:refs/heads/::)
 
 # Go back to the current branch when the script finishes.
-trap "git checkout $CURRENT_BRANCH" EXIT
+trap "run git checkout $CURRENT_BRANCH" EXIT
 
 function tag_to_image() {
 	local version=$(echo $tag | sed "s/^v//")
 	echo ${DOCKER_IMAGE}:$version
 }
 
+function run() {
+	echo "++ $@"
+	$@
+}
+
 echo "Building images..."
 for tag in $TAGS; do
-	echo "Building $tag"
-	git checkout $tag
-	docker build -t `tag_to_image $tag` .
+	echo
+	run git checkout $tag
+	run docker build -t `tag_to_image $tag` .
 done
 
 echo "Pushing images..."
 for tag in $TAGS; do
-	echo "Pushing $tag"
-	docker push `tag_to_image $tag`
+	echo
+	run docker push `tag_to_image $tag`
 done
